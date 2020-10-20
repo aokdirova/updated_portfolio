f8114b59d3aed53b5eeda18e1e94897d7ed832a8
aokdirova <67522548+aokdirova@users.noreply.github.com>
2020-10-20 14:24:54 +0200

    Removing scrolling of about and projects sections

--- src/js/sr.js
+++ src/js/sr.js
@@ -9,13 +9,13 @@ export default function() {
   };
   
   /* Section Title */
-  ScrollReveal().reveal('.section-title', 
-    assignProps(
-      {
-        delay:300,distance:'0px', 
-        origin:'bottom'
-      }, defaultProps)
-  );
+  // ScrollReveal().reveal('.section-title', 
+  //   assignProps(
+  //     {
+  //       delay:300,distance:'0px', 
+  //       origin:'bottom'
+  //     }, defaultProps)
+  // );
 
   /* Hero Section */
   ScrollReveal().reveal('.hero-title', 
@@ -35,45 +35,45 @@ export default function() {
   );
 
   /* About Section */
-  ScrollReveal().reveal('.about-wrapper__image', 
-    assignProps(
-      {
-        delay: 600,
-        origin: 'bottom'
-      }, defaultProps)
-  );
+  // ScrollReveal().reveal('.about-wrapper__image', 
+  //   assignProps(
+  //     {
+  //       delay: 600,
+  //       origin: 'bottom'
+  //     }, defaultProps)
+  // );
 
-  ScrollReveal().reveal('.about-wrapper__info', 
-    assignProps(
-      {
-        delay: 1000,
-        origin: window.innerWidth > 768 ? 'left' : 'bottom'
-      }, defaultProps)
-  );
+  // ScrollReveal().reveal('.about-wrapper__info', 
+  //   assignProps(
+  //     {
+  //       delay: 1000,
+  //       origin: window.innerWidth > 768 ? 'left' : 'bottom'
+  //     }, defaultProps)
+  // );
 
   /* Projects Section */
-  ScrollReveal().reveal('.project-wrapper__text', 
-    assignProps(
-      {
-        delay: 500,
-        origin: window.innerWidth > 768 ? 'left' : 'bottom'
-      }, defaultProps)
-  );
+  // ScrollReveal().reveal('.project-wrapper__text', 
+  //   assignProps(
+  //     {
+  //       delay: 500,
+  //       origin: window.innerWidth > 768 ? 'left' : 'bottom'
+  //     }, defaultProps)
+  // );
 
-  ScrollReveal().reveal('.project-wrapper__image', 
-    assignProps(
-      {
-        delay: 1000,
-        origin: window.innerWidth > 768 ? 'right' : 'bottom'
-      }, defaultProps)
-  );
+  // ScrollReveal().reveal('.project-wrapper__image', 
+  //   assignProps(
+  //     {
+  //       delay: 1000,
+  //       origin: window.innerWidth > 768 ? 'right' : 'bottom'
+  //     }, defaultProps)
+  // );
 
   /* Contact Section */
-  ScrollReveal().reveal('.contact-wrapper', 
-    assignProps(
-      {
-        delay: 800,
-        origin: 'bottom'
-      }, defaultProps)
-  );
+  // ScrollReveal().reveal('.contact-wrapper', 
+  //   assignProps(
+  //     {
+  //       delay: 800,
+  //       origin: 'bottom'
+  //     }, defaultProps)
+  // );
 }
